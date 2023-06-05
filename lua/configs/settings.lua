ff59a9492f127e8d0e658c41c6fc3d16475b82cb
Claudio Torcato <claudiotorcato@gmail.com>
2023-06-05 00:46:46 -0300

    Adição de novas configurações básicas

--- lua/configs/settings.lua
+++ lua/configs/settings.lua
@@ -1,6 +1,31 @@
-vim.cmd([[ set guicursor= ]])
 vim.cmd([[ set encoding=utf8 ]])
+-- mostrar a numeração das linhas
 vim.cmd([[ set nu! ]])
+-- mostrar a numeração das linhas relativas ao cursor
+vim.cmd([[ set relativenumber ]])
+-- permite a utilização do mouse em todos os modos
 vim.cmd([[ set mouse=a ]])
+-- menu para autocompletar na linha de comandos
 vim.cmd([[ set wildmenu ]])
+-- pergunta se deseja fechar o programa
 vim.cmd([[ set confirm ]])
+-- pesquisa incremental
+vim.cmd([[ set incsearch ]])
+-- permite mudar o titulo do terminal
+vim.cmd([[ set title ]])
+-- título do terminal
+vim.cmd([[ set titlestring=NVIM:\ %-25.55F\ %a%r%m titlelen=70 ]])
+vim.cmd([[ set t_Co=256 ]])
+vim.cmd([[ set shiftwidth=2 ]])
+vim.cmd([[ set softtabstop=2 ]])
+vim.cmd([[ set expandtab ]])
+vim.cmd([[ set shiftwidth=2 ]])
+vim.cmd([[ set softtabstop=2 ]])
+vim.cmd([[ set expandtab ]])
+vim.cmd([[ set guicursor= ]])
+vim.cmd([[ set cursorline ]])
+vim.cmd([[ syntax on ]])
+vim.cmd([[ set bg=dark ]])
+vim.cmd([[ set termguicolors ]])
+vim.cmd([[ colorscheme onedark ]])
+vim.cmd([[ set clipboard+=unnamedplus ]])
