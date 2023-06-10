0b6a55e6987d42678a3d6bcde2667fc3449dc660
Claudio Torcato <claudiotorcato@gmail.com>
2023-06-10 07:07:59 -0300

    Mais comentários nas configurações

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
-- Instalação e configuração do Noite https://github.com/folke/noice.nvim
+- Instalação e configuração do Noise https://github.com/folke/noice.nvim
+- Instalação e configuração do Which-Key
 
 ## [0.4.0] - 2023-06-09
 - Instalação do Telescope
--- lua/configs/settings.lua
+++ lua/configs/settings.lua
@@ -2,7 +2,7 @@ vim.cmd([[ set encoding=utf8 ]])
 -- mostrar a numeração das linhas
 vim.cmd([[ set nu! ]])
 -- mostrar a numeração das linhas relativas ao cursor
-vim.cmd([[ set relativenumber ]])
+vim.cmd([[ set relativenumber ]])                              
 -- permite a utilização do mouse em todos os modos
 vim.cmd([[ set mouse=a ]])
 -- menu para autocompletar na linha de comandos
@@ -16,12 +16,31 @@ vim.cmd([[ set title ]])
 -- título do terminal
 vim.cmd([[ set titlestring=NVIM:\ %-25.55F\ %a%r%m titlelen=70 ]])
 vim.cmd([[ set t_Co=256 ]])
-vim.cmd([[ set shiftwidth=2 ]])
-vim.cmd([[ set softtabstop=2 ]])
+-- **********************
+-- Use o comando :retab para reajustar todos os tamanhos de tab
+-- do arquivo
+-- **********************
+-- Usa espaços para identar mesmo quando pressionamos Tab
 vim.cmd([[ set expandtab ]])
+-- Controla o número de espaços que são inseridos para identação.
+-- Quando você tecla Enter para a próxima linha e espera que seja
+-- identado (um ou mais tabs inseridos automaticamente). No caso
+-- abaixo, a identação acontecerá com 2 espaços.
 vim.cmd([[ set shiftwidth=2 ]])
+-- Insere o número especificado de espaços quando teclando a tecla Tab.
+-- É parecido com o tabstop mas com outras particularidades:
+-- * Seu valor sobrescreve o informado por tabstop;
+-- * Ele também remove o mesmo número de espaços quando se usa Backspace;
+-- * Continuará a inserir espaços mesmo se o noexpandtab está habilitado,
+-- que é era para inserir tabs ao teclar a tecla Tab;
+-- * Um valor zero desabilita e um valor negativo faz recuar o valor de
+-- shifwidth.
 vim.cmd([[ set softtabstop=2 ]])
-vim.cmd([[ set expandtab ]])
+-- Controla o tamanho do Tab quando expandtab está desabilitado e
+-- o número de caracteres espaços a serem inseridos quando expandtab
+-- está habilitado. No caso abaixo, 2 espaços serão inseridos toda
+-- vez que digitar a tecla tab.
+vim.cmd([[ set tabstop=2 ]])
 vim.cmd([[ set guicursor= ]])
 vim.cmd([[ set cursorline ]])
 vim.cmd([[ syntax on ]])
