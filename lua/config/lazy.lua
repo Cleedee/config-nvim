b74cfa06c053776455ec94f2e416d5b87c9d99c9
Claudio Torcato <claudiotorcato@gmail.com>
2026-03-04 16:25:20 -0300

    Retirando markdown-preview.nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -55,16 +55,6 @@ require("lazy").setup({
 	-- Visualize buffers as tabs
 	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
 
-	-- Preview markdown live in web browser
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
-
 	-- LSP zero
 	{
 		"VonHeikemen/lsp-zero.nvim",
