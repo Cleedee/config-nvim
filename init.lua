b77b6236291460ff9d1bd2c6c5a886df1362dc9a
Claudio Torcato <claudiotorcato@gmail.com>
2026-03-04 14:21:23 -0300

    Tentativa de fazer o lspconfig-nvim funcionar

--- init.lua
+++ init.lua
@@ -3,3 +3,23 @@ require("options")
 require("config.lazy")
 require("plugins.keymaps")
 require("plugins.options")
+
+vim.lsp.config("*", {
+	on_attach = function(client, bufnr)
+		-- overwrites omnifunc/tagfunc set by some Python plugins to the
+		-- default values for LSP
+		vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
+		vim.api.nvim_set_option_value("tagfunc", "v:lua.vim.lsp.tagfunc", { buf = bufnr })
+
+		vim.keymap.set({ "n", "v" }, "K", vim.lsp.buf.hover, { buffer = bufnr })
+		vim.keymap.set({ "n", "v" }, "<F4>", vim.lsp.buf.format, { buffer = bufnr })
+		vim.keymap.set("n", "gu", vim.lsp.buf.references, { buffer = bufnr })
+		vim.keymap.set("n", "gr", vim.lsp.buf.rename, { buffer = bufnr })
+
+		vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+			signs = true,
+			underline = true,
+			virtual_text = true,
+		})
+	end,
+})
